b04e176e5589c478afe9dd6902c2e9554cc7deb8
rezahy <reza1997hy@yahoo.com>
2025-04-28 01:32:14 +0430

    add-theme

--- src/main.tsx
+++ src/main.tsx
@@ -3,13 +3,16 @@ import "./index.css";
 import App from "./App.tsx";
 import { BrowserRouter as Router } from "react-router-dom";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { ThemeProvider } from "./providers/theme-provider.tsx";
 
 const queryClient = new QueryClient();
 
 createRoot(document.getElementById("root")!).render(
 	<QueryClientProvider client={queryClient}>
 		<Router>
-			<App />
+			<ThemeProvider defaultTheme="dark" storageKey="vite-ui-theme">
+				<App />
+			</ThemeProvider>
 		</Router>
 	</QueryClientProvider>
 );
