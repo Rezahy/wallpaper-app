cb77c5f3a8ab53f795bb8a2172bc2061328a0824
rezahy <reza1997hy@yahoo.com>
2025-04-30 09:59:54 +0430

    add-favorite-wallpapers-page

--- src/app/favorite/page.tsx
+++ src/app/favorite/page.tsx
@@ -1,4 +1,29 @@
+import EmptyView from "@/components/empty-view";
+import WallpaperList from "@/components/wallpaper-list";
+import useWallpaper from "@/stores/wallpaper";
+import { motion } from "motion/react";
+
 const FavoritePage = () => {
-	return <div>FavoritesPage</div>;
+	const favoriteWallpapers = useWallpaper((state) => state.favoriteWallpapers);
+	return (
+		<section>
+			<header>
+				<h1 className="text-2xl font-semibold pt-7 pb-5">
+					Favorite Wallpapers
+				</h1>
+			</header>
+			{favoriteWallpapers.length > 0 ? (
+				<WallpaperList data={favoriteWallpapers} />
+			) : (
+				<motion.div
+					initial={{ opacity: 0 }}
+					animate={{ opacity: 1 }}
+					transition={{ duration: 0.5 }}
+				>
+					<EmptyView>Your Favorite Wallpaper List is Empty</EmptyView>
+				</motion.div>
+			)}
+		</section>
+	);
 };
 export default FavoritePage;
