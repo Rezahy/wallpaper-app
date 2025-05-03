706adf3316f354b88915aac732e7d0fd9f70860e
rezahy <reza1997hy@yahoo.com>
2025-05-03 21:51:43 +0430

    fix-wallpaper-list-item-animation

--- src/components/wallpaper-list-item.tsx
+++ src/components/wallpaper-list-item.tsx
@@ -6,6 +6,8 @@ import LikeWallpaperButton from "./like-wallpaper-button";
 import { Link } from "react-router-dom";
 import { LazyLoadImage } from "react-lazy-load-image-component";
 import "react-lazy-load-image-component/src/effects/blur.css";
+import { useState } from "react";
+import { Skeleton } from "./ui/skeleton";
 
 const MotionComponent = motion.create(Link);
 type WallpaperListItemProps = {
@@ -18,6 +20,7 @@ const WallpaperListItem = ({
 	index,
 	animated,
 }: WallpaperListItemProps) => {
+	const [isImageLoaded, setIsImageLoaded] = useState(false);
 	return (
 		<MotionComponent
 			to={`/wallpaper/${wallpaper.id}`}
@@ -37,24 +40,41 @@ const WallpaperListItem = ({
 					loading="lazy"
 					className="h-[350px] object-cover group-hover:scale-115 transition-all duration-500"
 				/> */}
-				<LazyLoadImage
-					src={wallpaper.webformatURL}
-					alt={`wallpaper - ${wallpaper.id}`}
-					loading="lazy"
-					effect="blur"
-					className="h-[350px] w-full object-cover group-hover:scale-115 transition-all duration-500"
-					style={{
-						transitionProperty: "all",
-						transitionTimingFunction:
-							"var(--tw-ease, var(--default-transition-timing-function)",
-						transitionDuration: "500ms",
-					}}
-				/>
-				<span className="absolute top-0 left-0 right-0 h-[60px] opacity-0 transition-opacity duration-400 group-hover:opacity-100 bg-gradient-to-b from-[#191b26]/90 from-0% to-transparent to-100%"></span>
-				<div className="absolute top-2 left-2 space-x-3 opacity-0 group-hover:opacity-100 transition-opacity duration-400">
-					<SaveWallpaperButton wallpaper={wallpaper} />
-					<LikeWallpaperButton wallpaper={wallpaper} />
-				</div>
+
+				{isImageLoaded ? (
+					<LazyLoadImage
+						src={wallpaper.webformatURL}
+						alt={`wallpaper - ${wallpaper.id}`}
+						loading="lazy"
+						effect="blur"
+						className="h-[350px] w-full object-cover group-hover:scale-115 transition-all duration-500"
+						style={{
+							transitionProperty: "all",
+							transitionTimingFunction:
+								"var(--tw-ease, var(--default-transition-timing-function)",
+							transitionDuration: "500ms",
+						}}
+					/>
+				) : (
+					<Skeleton className="rounded-xl shadow-sm h-[350px]">
+						<img
+							src={wallpaper.webformatURL}
+							alt=""
+							className="hidden"
+							onLoad={() => setIsImageLoaded(true)}
+						/>
+					</Skeleton>
+				)}
+
+				{isImageLoaded && (
+					<>
+						<span className="absolute top-0 left-0 right-0 h-[60px] opacity-0 transition-opacity duration-400 group-hover:opacity-100 bg-gradient-to-b from-[#191b26]/90 from-0% to-transparent to-100%"></span>
+						<div className="absolute top-2 left-2 space-x-3 opacity-0 group-hover:opacity-100 transition-opacity duration-400">
+							<SaveWallpaperButton wallpaper={wallpaper} />
+							<LikeWallpaperButton wallpaper={wallpaper} />
+						</div>
+					</>
+				)}
 			</Card>
 		</MotionComponent>
 	);
